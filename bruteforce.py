e57e1997b43dace4212abda65dca69d1e8cd292d
baxtermaia5000 <117203987+baxtermaia5000@users.noreply.github.com>
2023-12-11 11:50:25 -0500

    Update bruteforce.py
    Added comments so people can know what went on in my head when I coded this shit

--- bruteforce.py
+++ bruteforce.py
@@ -1,6 +1,7 @@
 import requests as rq
 import random as rand
 mode = input("Loud mode?\n>>> ")
+#define stuff for request (cURL converter .com)
 cookies = {
     '__stripe_mid': '32dcd2aa-4803-48c6-8fb4-110d7c415f1ddf56a4',
     '__stripe_sid': 'c40e1442-e78a-40b3-9b38-c4b7706b489b62cc96',
@@ -20,31 +21,40 @@ headers = {
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
 }
-
-file=open("hits.txt","w")
+#open file and prep loop
+file=open("hits.txt", "w")
 code=79536
 checked=[]
+
+#MAIN LOOP
+
 while True:
+    #redefine code var for requests
     json_data = {
         'code': str(code),
     }
+    #add code to list for duplicate rerolling
     checked.append(code)
+    #print more shit if in loud mode
     if mode=="y":
         print(f'Searching {code}...')
+    #send post data
     response = rq.post(
         'https://www.gimkit.com/api/matchmaker/find-info-from-code',
         cookies=cookies,
         headers=headers,
         json=json_data,
     )
-
+    #ignore if invalid, print and append to file if valid code
     if str(response)=="<Response [500]>":
         #print(str(response) + str(code))
         pass
     else:
         print(f'Success at {code}!')
         file.write(f'{code}\n')
+    #reroll code using randint()
     code=rand.randint(0,999999)
+    #Make sure new code isn't a duplicate
     while code in checked:
         code=rand.randint(0,999999)
         print("rerolled dupe")
